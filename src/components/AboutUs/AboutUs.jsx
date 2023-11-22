2ac70e1061ca5192afdd5fdcda37250390c6216e
Murix_hyper <murad.babaiev@nure.ua>
2023-11-22 02:17:27 +0100

    blog page was added

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -10,20 +10,26 @@ const AboutUs = () => {
         <h2>About Comet.S</h2>
       </div>
       <div className="container">
-        <div className="text">
+        <div className="text left">
           <p>CometS is far from an unusual place, it does not look like a simple pub with a standard menu and fixed prices.</p>
         </div>
-        <div className="image">
-          <img src={rightimg} alt="" />
+        <div className="imagelr">
+          <img src={leftimg} alt="" />
         </div>
       </div>
       <div className="container">
-        <div className="inage">
+        <div className="imagelr">
+          <img src={rightimg} alt="" />
+        </div>
+        <div className="text right">
           <p> We have developed a special pricing system that will change in real time, which will surely surprise visitors and allow them to choose a drink to their liking.</p>
         </div>
-        <div className="text">
-          <img src={leftimg} alt="" />
-        </div>
+      </div>
+      <div className="content">
+        <p>We specialize in turning your boring evening into something more than just a trip to the bar. Large screens throughout the establishment broadcasting the demand for various drinks will create a special atmosphere of the bar.</p>
+      </div>
+      <div className="mission">
+        <p>Our mission: To make your evening special and make you think about what you will drink.</p>
       </div>
     </div>
   );
